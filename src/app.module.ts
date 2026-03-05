c664a9cf41dd63bbfb806609ecb83b505aab515b
Cristiano Barbosa <cristianobr188@gmail.com>
2026-03-05 15:48:13 -0300

    banco-neon

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,31 @@ import { MusicasModule } from './musicas/musicas.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MusicasModule, 
+  imports: [MusicasModule,
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'db.orkjcediebvholwhqtxt.supabase.co',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'CristianoC1c@barbosa',
+    //   database: 'postgres',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    //   ssl: {
+    //     rejectUnauthorized: false,
+    //   },
+    // })
     TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'musicas.db',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-  }),
+      type: 'postgres',
+      url: 'postgresql://neondb_owner:npg_rtQeYcX4Kxi5@ep-spring-sea-ailldw2h-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
