7fd7b20adeb8259b8a796b4ade1567a99e715bb8
Cristiano de Oliveira Barbosa <cristianobr188@gmail.com>
2026-03-04 21:26:05 -0300

    teste new main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,44 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+  
+//   app.enableCors();
+
+//   const config = new DocumentBuilder()
+//     .setTitle('API de Músicas')
+//     .setDescription('Documentação da API da banda')
+//     .setVersion('1.0')
+//     .build();
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, document);
+
+
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('API de Músicas')
-    .setDescription('Documentação da API da banda')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'https://worship-set.vercel.app'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  });
 
-  app.enableCors();
+   const config = new DocumentBuilder()
+     .setTitle('API de Músicas')
+     .setDescription('Documentação da API da banda')
+     .setVersion('1.0')
+     .build();
+   const document = SwaggerModule.createDocument(app, config);
+   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
 bootstrap();
