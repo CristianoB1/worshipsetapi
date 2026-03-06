a8a358b3332316a8667edf7aa0a81af5ea68a473
Cristiano Barbosa <cristianobr188@gmail.com>
2026-03-06 08:54:05 -0300

    upp

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [MusicasModule,
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: 'db.orkjcediebvholwhqtxt.supabase.co',
-    //   port: 5432,
-    //   username: 'postgres',
-    //   password: 'CristianoC1c@barbosa',
-    //   database: 'postgres',
-    //   autoLoadEntities: true,
-    //   synchronize: true,
-    //   ssl: {
-    //     rejectUnauthorized: false,
-    //   },
-    // })
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: 'postgresql://neondb_owner:npg_rtQeYcX4Kxi5@ep-spring-sea-ailldw2h-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
